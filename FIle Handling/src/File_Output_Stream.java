dbc3ac4102b4cdbe3abed91967e3d5c23f4d8dfe
DEEPAK UMMIDI <98415828+DeepakUmmidi28@users.noreply.github.com>
2024-09-10 12:36:48 +0530

    File handling in java

---
+++ FIle Handling/src/File_Output_Stream.java
@@ -0,0 +1,17 @@
+import java.io.FileOutputStream;
+
+public class File_Output_Stream {
+    public static void main(String[] args) throws Exception{
+        FileOutputStream fos=new FileOutputStream("Test.txt");
+        // String str=new String("Deepak");
+        // byte b[]=str.getBytes();
+        // for(byte x:b)
+        // fos.write(x);
+        // fos.close();
+        
+        String str=new String("Deepak Ummidi");
+        byte b[]=str.getBytes();
+        fos.write(b, 7, str.length()-8);
+        fos.close();
+    }
+}
